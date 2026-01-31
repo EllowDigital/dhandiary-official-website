24f78ed3ab887b9f3c5bb1b6bc8709040346f67b
Sarwan Yadav <sarwanyadav6174@gmail.com>
2026-01-31 13:33:38 +0530

    fix: correct pageSEO import in Terms page after refactor

--- src/pages/Terms.tsx
+++ src/pages/Terms.tsx
@@ -1,5 +1,6 @@
 import Layout from "@/components/layout/Layout";
-import SEOHead, { pageSEO } from "@/components/shared/SEOHead";
+import SEOHead from "@/components/shared/SEOHead";
+import { pageSEO } from "@/components/shared/SEOHead.constants";
 import PageHero from "@/components/shared/PageHero";
 import SectionCard from "@/components/shared/SectionCard";
 import AnimatedSection from "@/components/shared/AnimatedSection";
